a2d08a79603d92c7004c65947fec4cbb2ac247bb
MAJIIDMN <muhammadnurmajiid22@gmail.com>
2026-03-22 14:53:00 +0700

    initial commit

---
+++ src/destroyer_drive.cpp
@@ -0,0 +1,11 @@
+#include <rclcpp/rclcpp.hpp>
+
+class DestroyerDrive : public rclcpp::Node
+{
+    //TODO: Implementasi node drive_mode_destroyer
+};
+
+int main(int argc, char * argv[])
+{
+  return 0;
+}
